c1194e220d92c5b4829abd035485a0672b4042e4
Greg Priday <greg@siteorigin.com>
2023-05-27 18:09:27 +0200

    Added config

--- config/scout-qdrant.php
+++ config/scout-qdrant.php
@@ -6,6 +6,7 @@ return [
     'qdrant' => [
         'host' => env('QDRANT_HOST', 'http://localhost'),
         'key' => env('QDRANT_API_KEY', null),
+        'storage' => env('QDRANT_STORAGE', 'database/qdrant'),
     ],
     'vectorizer' => env('QDRANT_VECTORIZER', 'openai'),
 ];
